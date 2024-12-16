a5b48be3c1f74fb69771ad6370397097a39fb18d
YSAAVE1 <YSAAVE1@bancodebogota.com.co>
2024-12-16 11:42:28 -0500

    Add files via upload

---
+++ src/test/scala/privateApi/PostCreateproductaccountopentwo.scala
@@ -0,0 +1,39 @@
+package Api_PrivadaPOST
+
+import io.gatling.core.Predef._
+import objects.MethodsHttp.post
+import utilities.{BuildRequest, EnvironmentValues, RequestParams}
+
+class PostCreateproductaccountopentwo extends Simulation{
+
+  val env = new EnvironmentValues()
+  val params= new RequestParams()
+
+  params.baseUrl="https://api.labdigbdbstgcd.com"
+  params.pathUrl="/accounts-management/v2/product/account"
+  params.headers=Map(
+    "X-CustIdentType" -> "#{tipoid}",
+    "X-CustIdentNum" -> "#{documento}",
+    "X-IPAddr" -> "192.168.0.1",
+    "X-RqUID" -> "a9e738b0-6369-47d9-b563-8dce7bb300fa",
+    "X-Name" -> "CuentaDeAhorros",
+    "X-Channel" -> "Web",
+    "X-DigRequest" -> "123",
+    "X-AccessToken" -> "accessToken",
+    "X-CustType" -> "ACTIVE",
+    "x-api-key" -> "uGTRR0b1R96HW9zyPb8hh3PhmiYufJyb4OS6WO1r",
+    "Content-Type" -> "application/json"
+  )
+  params.method=post
+  params.requestName="CreateProductAccount"
+  params.statusExpected=201
+  params.scenarioName="Crear Cuentas"
+  params.templateJson= "data/crearBodyAccount.json"
+  params.feederCsv = Array("data/datoscliente.csv")
+
+  val request = new BuildRequest(params)
+
+  setUp(
+    request.scn.inject(constantUsersPerSec(10).during(5).randomized)
+  ).protocols(request.httpProtocol)
+}
